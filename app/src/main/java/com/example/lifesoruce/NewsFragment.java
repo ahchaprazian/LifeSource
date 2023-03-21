6a1ff78019ad4b6c4087b2ffd45ec6fe3a6d2604
Alexan Chaprazian <ahchaprazian@gmail.com>
2023-03-21 15:15:11 -0400

    removed some unnessary commented code

--- app/src/main/java/com/example/lifesoruce/NewsFragment.java
+++ app/src/main/java/com/example/lifesoruce/NewsFragment.java
@@ -36,10 +36,6 @@ public class NewsFragment extends Fragment {
     private ArrayList<NewsArticle> mArticleList;
     private ArticleAdapter mArticleAdapter;
 
-    /*public void onAttach(Context context) {
-        super.onAttach(context);
-        AndroidNetworking.initialize(context);
-    }*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -60,64 +56,6 @@ public class NewsFragment extends Fragment {
         mArticleList = new ArrayList<>();
 
         get_news_from_api();
-
-        //AndroidNetworking.initialize(getApplicationContext());
-        /*NewsApiClient newsApiClient = new NewsApiClient("NEWS_API_KEY");
-
-        newsApiClient.getEverything(
-                new EverythingRequest.Builder()
-                        .q("trump")
-                        .build(),
-                new NewsApiClient.ArticlesResponseCallback() {
-                    @Override
-                    public void onSuccess(ArticleResponse response) {
-                        System.out.println(response.getArticles().get(0).getTitle());
-                    }
-
-                    @Override
-                    public void onFailure(Throwable throwable) {
-                        System.out.println(throwable.getMessage());
-                    }
-                }
-        );
-
-        // /v2/top-headlines
-        newsApiClient.getTopHeadlines(
-                new TopHeadlinesRequest.Builder()
-                        .q("bitcoin")
-                        .language("en")
-                        .build(),
-                new NewsApiClient.ArticlesResponseCallback() {
-                    @Override
-                    public void onSuccess(ArticleResponse response) {
-                        System.out.println(response.getArticles().get(0).getTitle());
-                    }
-
-                    @Override
-                    public void onFailure(Throwable throwable) {
-                        System.out.println(throwable.getMessage());
-                    }
-                }
-        );
-
-        // /v2/top-headlines/sources
-        newsApiClient.getSources(
-                new SourcesRequest.Builder()
-                        .language("en")
-                        .country("us")
-                        .build(),
-                new NewsApiClient.SourcesCallback() {
-                    @Override
-                    public void onSuccess(SourcesResponse response) {
-                        System.out.println(response.getSources().get(0).getName());
-                    }
-
-                    @Override
-                    public void onFailure(Throwable throwable) {
-                        System.out.println(throwable.getMessage());
-                    }
-                }
-        );*/
 
         return view;
     }
