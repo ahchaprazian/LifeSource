cc991c66336509feda2a25b1d17c6a26e4896e49
Alex Dang <alexdang0308@yahoo.com>
2023-03-21 04:32:52 -0400

    added google maps sdk with basic marker for UML

--- app/src/main/java/com/example/lifesoruce/MapFragment.java
+++ app/src/main/java/com/example/lifesoruce/MapFragment.java
@@ -2,6 +2,8 @@ package com.example.lifesoruce;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,9 +11,27 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.lifesoruce.databinding.FragmentMapBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends Fragment {
     private FragmentMapBinding binding;
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(@NonNull GoogleMap googleMap) {
+            googleMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(42.6553, -71.3247))
+                    .title("Marker"));
+
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(42.6553, -71.3247), 15f));
+        }
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -20,6 +40,17 @@ public class MapFragment extends Fragment {
         View view = binding.getRoot();
 
         return view;
+
+        //return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
     }
 
     @Override
