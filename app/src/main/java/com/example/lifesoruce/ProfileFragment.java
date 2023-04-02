0e790b9b3f758c71355198c3966545a075e17d83
Alexan Chaprazian <ahchaprazian@gmail.com>
2023-04-02 12:17:31 -0400

    mostly functional list for reminders

--- app/src/main/java/com/example/lifesoruce/ProfileFragment.java
+++ app/src/main/java/com/example/lifesoruce/ProfileFragment.java
@@ -4,19 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.lifesoruce.databinding.FragmentProfileBinding;
 
+import java.util.ArrayList;
+
 public class ProfileFragment extends Fragment {
     private FragmentProfileBinding binding;
+    private ListView listView;
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+
+        listView = binding.listView;
+        items = new ArrayList<>();
 
         /*
         * When the button in the bottom corner is clicked
@@ -42,7 +52,9 @@ public class ProfileFragment extends Fragment {
                 String text = binding.editText.getText().toString();
 
                 // TODO: Add text to list
-
+                items.add(text);
+                adapter = new ArrayAdapter<>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, items);
+                listView.setAdapter((adapter));
                 // Hide popupLayout
                 binding.popupLayout.setVisibility(View.GONE);
             }
